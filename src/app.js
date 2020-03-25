e94c09491181886e2f87ee593348bbabde67f396
leny <info@flatland.be>
2020-03-25 14:09:09 +0100

    Basic react-bootstrap & app structure

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import RootComponent from "./components/root";
 
--- src/components/root.js
+++ src/components/root.js
@@ -6,8 +6,44 @@
  * started at 25/03/2020
  */
 
-import React from "react";
+import React, {useState, useCallback} from "react";
+import {Container, Row, Col, Card, Button} from "react-bootstrap";
+import classnames from "classnames";
 
-const RootComponent = () => <p>{"Hello, world!"}</p>;
+const RootComponent = () => {
+    const [showMe, setShowMe] = useState(false);
+
+    const handleShowMe = useCallback(() => setShowMe(true), [setShowMe]);
+
+    let $content;
+
+    if (showMe) {
+        $content = <p>{"I will, I will."}</p>;
+    } else {
+        $content = (
+            <div className={classnames("mt-3", "text-center")}>
+                <Button variant={"outline-primary"} onClick={handleShowMe}>
+                    {"No, show me!"}
+                </Button>
+            </div>
+        );
+    }
+
+    return (
+        <Container className={"my-3"}>
+            <Row>
+                <Col sm={{span: 4, offset: 4}}>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>{"Hello!"}</Card.Title>
+                            <Card.Text>{"Do you know who I am?"}</Card.Text>
+                            {$content}
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
 
 export default RootComponent;
