e822dcbae6eb8e932aec6e98685ca1391395d2b7
leny <info@flatland.be>
2020-03-25 13:51:52 +0100

    Install & setup basic react

--- src/app.js
+++ src/app.js
@@ -6,4 +6,9 @@
  * started at 25/03/2020
  */
 
-console.warn("I'm ready!");
+import React from "react";
+import ReactDOM from "react-dom";
+
+import RootComponent from "./components/root";
+
+ReactDOM.render(<RootComponent />, document.querySelector("#app"));
---
+++ src/components/root.js
@@ -0,0 +1,13 @@
+/* becodeorg/stream
+ *
+ * /src/components/root.js - Root Component
+ *
+ * coded by leny@BeCode
+ * started at 25/03/2020
+ */
+
+import React from "react";
+
+const RootComponent = () => <p>{"Hello, world!"}</p>;
+
+export default RootComponent;
